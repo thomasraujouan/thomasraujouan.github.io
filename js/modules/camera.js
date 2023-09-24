946af5182af0e905449c8ef32f4efb66909cc6ff
Thomas <thomas.raujouan@gmail.com>
2023-09-24 10:54:56 +0900

    dressed catenoid 3v updated

--- js/modules/camera.js
+++ js/modules/camera.js
@@ -5,12 +5,13 @@ const makeCamera = (
   aspect = 1,
   near = 0.001,
   far = 1000,
-  initialTarget = { x: 0, y: 0, z: 0 }
+  initialTarget = { x: 0, y: 0, z: 0 },
+  position = { x: 3, y: 2, z: 3 }
 ) => {
   const camera = new PerspectiveCamera(fov, aspect, near, far);
-  camera.position.x = 3;
-  camera.position.y = 2;
-  camera.position.z = 3;
+  camera.position.x = position.x;
+  camera.position.y = position.y;
+  camera.position.z = position.z;
   camera.initialTarget = initialTarget;
   camera.lookAt(initialTarget.x, initialTarget.y, initialTarget.z);
   return camera;
