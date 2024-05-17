1e0021e2e530b2f44232f9e7fba67f778c80d1fb
Thomas Raujouan <thomas.raujouan@gmail.com>
2024-05-17 15:32:58 +0900

    added cone in Minkowski space

--- darboux-minkowski/main.js
+++ darboux-minkowski/main.js
@@ -38,7 +38,29 @@ for (let index = 0; index < pieces.length; index++) {
   scene.add(pieces[index]);
 }
 
+/* LIGHT CONE */
+const coneRadius = 2.3;
+const geometry = new THREE.ConeGeometry(
+  coneRadius, 
+  coneRadius, 
+  16, 
+  1, 
+  true); 
+const wireframe = new THREE.WireframeGeometry( geometry );
+const cone1 = new THREE.LineSegments( wireframe );
+cone1.translateY(-0.5*coneRadius);
+const cone2 = new THREE.LineSegments( wireframe );
+cone2.translateY(0.5*coneRadius);
+cone2.rotateX(Math.PI);
+const cones = [cone1, cone2];
+cones.forEach(cone => {
+  cone.material.depthTest = true;
+  cone.material.transparent = true;
+  cone.material.opacity = 0.5;
+  scene.add( cone )
+});
 
+/* CAMERA */
 const fov = 30;
 const radius = computeRadius(obj);
 const distance = fittingDistance(fov, radius);
