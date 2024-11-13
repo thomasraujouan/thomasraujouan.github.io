172bbb2a1c9cdfa0223e124848667092fbb4b0da
Thomas <thomas.raujouan@gmail.com>
2024-11-13 16:53:00 +0900

    dressed helicoid and enneper: done todo: make more beautiful objs

---
+++ dressed-catenoids/extras/dressed-enneper/app.js
@@ -0,0 +1,24 @@
+import { Viewer } from "/dressed-catenoids/threejsViewer.js";
+
+
+// Configuration (specific to each surface)
+const config = {
+    objSource: "./dressed-enneper.obj",
+    textureSource: "./texture.svg",
+    backgroundColor: "white", // White for this page
+    numberOfCopies: 3,
+    globalTransform:
+        (obj) => {
+            obj.rotateZ(-Math.PI / 2);
+            obj.rotateX(Math.PI / 2);
+        },
+    copiesTransforms: [],
+    cameraPosition: { x: 0, y: 0, z: 30 },
+    // Add more params as needed
+};
+
+// Get the container DOM element
+const container = document.body;
+
+// Initialize the viewer with the container and configuration
+const viewer = new Viewer(container, config);
