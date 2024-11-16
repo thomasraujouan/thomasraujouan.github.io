1725980d1b7497787858d5ff24a28526a7d824ef
Thomas <thomas.raujouan@gmail.com>
2024-11-16 17:55:15 +0900

    better 1plane dressed catenoid todo: adapt to the other surfaces

--- dressed-catenoids/E3/1-plane/vertical/app.js
+++ dressed-catenoids/E3/1-plane/vertical/app.js
@@ -3,25 +3,34 @@ import { Viewer } from "/dressed-catenoids/threejsViewer.js";
 
 // Configuration (specific to each surface)
 const config = {
+    // objSource: "./e3-1v-mathematica.obj",
     objSource: "./e3-1v.obj",
     textureSource: "./texture.svg",
+    textureAnisotropy: 4, // usually a power of 2
     backgroundColor: "white", // White for this page
-    numberOfCopies: 3,
+    surfaceColor: 0xcc0000,
+    // numberOfCopies: 3,
     globalTransform:
         (obj) => {
-            obj.rotateY((1 * Math.PI) / 8);
-            obj.rotateX((-0 * Math.PI) / 16);
-            obj.rotateZ((0 * Math.PI) / 2);
+            obj.rotateY((3 * Math.PI) / 8);
+            obj.rotateX((0 * Math.PI) / 16);
+            obj.rotateZ((-8 * Math.PI) / 16);
         },
     copiesTransforms: [
-        (obj) => obj.scale.y = -1,
-        (obj) => obj.rotateX(Math.PI),
+        (obj) => obj.scale.z = -1,
+        (obj) => obj.rotateY(Math.PI),
         (obj) => {
-            obj.scale.y = -1;
-            obj.rotateX(Math.PI);
+            obj.scale.z = -1;
+            obj.rotateY(Math.PI);
         }
     ],
-    // cameraPosition: { x: 0, y: 0, z: 40 }
+    cameraPosition: { x: 30, y: 0, z: 0 },
+    cameraTransform:
+        (camera) => {
+            // camera.rotateY((1 * Math.PI) / 8);
+            // camera.rotateX((1 * Math.PI) / 16);
+            // camera.rotateZ((-1 * Math.PI) / 2);
+        },
     // Add more params as needed
 };
 
